0ce402b8ba04f83cdb442be122590cb68b11dd0b
Victor K. Opare-Addo <34301257+voakVictor@users.noreply.github.com>
2026-04-01 16:16:18 +0000

    Update background image URL in car_price_predictor

--- car_price_predictor.py
+++ car_price_predictor.py
@@ -20,7 +20,7 @@ def add_bg_from_url():
         f"""
         <style>
         .stApp {{
-            background-image: url("https://www.vecteezy.com/photo/37371661-ai-generated-a-red-car-among-grey-cars-marketing-concept-ai-generative");
+            background-image: url("https://www.vecteezy.com/photo/35827942-ai-generated-the-car-in-water-splashing");
             background-size: cover;
             background-repeat: no-repeat;
             background-attachment: fixed;
